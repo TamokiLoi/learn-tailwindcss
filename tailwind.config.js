0711e878de8ce029ada8c7dc932eb123c9aee864
loinlt <loinlt.stageid.app>
2021-02-25 16:04:17 +0700

    update src-learn-tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.jsx", "./index.html"],
+    options: {
+      whitelist: [],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: "#3fbaeb",
+          DEFAULT: "#0fa9e6",
+          dark: "#0c87b8",
+        }
+      },
+      fontFamily: {
+        headline: "Poppins, sans-serif"
+      }
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+}
